ec029ed36ec489a8c16df294cdb9bf91426fe3b6
wojciechjsky <jablonski91w@gmail.com>
2020-04-09 16:31:55 +0200

    Working printing table with field input

--- game_board.py
+++ game_board.py
@@ -4,21 +4,23 @@ class Board:
         self.height = height
 
     def create_board(self):
-        self.game_board = [['0']*self.width] * self.height
+        self.game_board_in_class = [['0'] * self.width for i in range(self.height)]
         self.player = 'X'
-        # print(self.game_board)
+
+    def update_board(self, x, y, sign_on_map):
+        self.game_board_in_class[x][y] = sign_on_map
 
     def print_board(self):
         overscore = "\u203e"
         # TOP
 
         letters_position = f"{' '*15}"
-        for item in range(len(self.game_board)):
-            letters_position += chr(65+item%26) + ' '*2
+        for item in range(len(self.game_board_in_class)):
+            letters_position += chr(65 + item % 26) + ' '*2
         print('\n', letters_position + '\n', f"{' '*11}{'_'*(self.width*3+3)}", sep='')
         # MIDDLE
         middle_fileds = ''
-        for i, list_of_fields in enumerate(self.game_board):
+        for i, list_of_fields in enumerate(self.game_board_in_class):
             if i == 0:
                 middle_fileds += f"{' '*6}{i+1}.{' '*3}|{self.player}|"
             elif i == 1:
@@ -30,12 +32,11 @@ class Board:
                 if field == '0':
                     middle_fileds += '   '
                 else:
-                    middle_fileds += field
+                    middle_fileds = middle_fileds + ' ' + field + ' '
          
-
-            if i == len(self.game_board)-2:
+            if i == len(self.game_board_in_class)-2:
                 middle_fileds += '|_\n'
-            elif i == len(self.game_board)-1:
+            elif i == len(self.game_board_in_class)-1:
                 middle_fileds += '| |'
             else:
                 middle_fileds += '|\n'
--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import engine
 import ui
 import game_board
 
+# pip install keyboard
+
 PLAYER_ICON = '@'
 PLAYER_START_X = 3
 PLAYER_START_Y = 3
@@ -31,9 +33,23 @@ def main():
     #          4. About\n''') 
     # For now just prototype of menu
     my_board = game_board.Board(BOARD_WIDTH, BOARD_HEIGHT)
- 
     my_board.create_board()
+    print(my_board.game_board_in_class)
+    x = 2
+    y = 3
+    sign_on_map = 'M'
+    my_board.update_board(x, y, sign_on_map)
     my_board.print_board()
+    # print(my_board.game_board_in_class)
+
+    # if keyboard.on_press_key('w', lambda _:print("You pressed p")):
+    #     print('Dzialam przysicnalem WWWWWWW')
+    # if keyboard.is_pressed('s'):
+    #     print('Dzialam przysicnalem ssssssss')
+    # if keyboard.is_pressed('a'):
+    #     print('Dzialam przysicnalem aaaaaaa')
+    # if keyboard.is_pressed('d'):
+    #     print('Dzialam przysicnalem ddddddd')
     # player = create_player()
     # board = engine.create_board(BOARD_WIDTH, BOARD_HEIGHT)
 
