6cd283da3d408a48a19a50080303284788475c50
wojciechjsky <jablonski91w@gmail.com>
2020-04-10 23:43:26 +0200

    Validation of heros_moves v1.0.1

--- game_board.py
+++ game_board.py
@@ -2,22 +2,20 @@ class Board:
     def __init__(self, width, height, p_sign):
         self.width = width
         self.height = height
-        self.pos_x = -1
-        self.pos_y = -1
         self.player_sign = p_sign
-
-    def create_board(self): # this maybe can go to the __init__
-        self.game_board_in_class = [['0'] * self.width for i in range(self.height)]
-        # self.game_board_in_class = [['0'] * self.width if i != 0 or i != self.height else ['0'] * (self.width+1) 
-        #                             for i in range(self.height)]
+        self.game_board_in_class = [[self.player_sign]] + [['0'] * self.width for i in range(self.height)] + [['0']]
+        self.game_board_in_class[self.pos_x][self.pos_y] = self.player_sign
         
+    pos_x = 0
+    pos_y = 0
 
 
     def update_board(self, pos_x, pos_y):
-        self.game_board_in_class[pos_x][pos_y] = self.player_sign
         self.pos_x = pos_x
         self.pos_y = pos_y
-        # self.game_board_in_class[x][y] = sign_on_map
+        self.game_board_in_class[pos_x][pos_y] = self.player_sign
+
+
        
 
     def remove_player_track(self):
@@ -27,39 +25,50 @@ class Board:
 
     def print_board(self):
         overscore = "\u203e"
+        print(self.game_board_in_class)
         # TOP
 
-        letters_position = f"{' '*15}"
-        for item in range(len(self.game_board_in_class)):
-            letters_position += chr(65 + item % 26) + ' '*2
-        print('\n', letters_position + '\n', f"{' '*11}{'_'*(self.width*3+3)}", sep='')
+        letters_position = f"{' '*14}"
+        for item in range(self.width):
+            letters_position += chr(65 + item % 26)
+        print('\n', letters_position + '\n', f"{' '*12}{'_'*(self.width+3)}", sep='')
+
         # MIDDLE
-        middle_fileds = ''
         for i, list_of_fields in enumerate(self.game_board_in_class):
             if i == 0:
-                middle_fileds += f"{' '*6}{i+1}.{' '*3}|{self.player_sign}|"
+                middle_fileds = f"{' '*6}{1}.{' '*4}|"
             elif i == 1:
-                middle_fileds += f"{' '*6}{i+1}.{' '*3} {overscore}|"
+                middle_fileds += ''
+            elif i == 2:
+                middle_fileds += f"\n{' '*6}{i}.{' '*4}{overscore}|"
+
+            elif i == len(self.game_board_in_class)-1:
+                middle_fileds += ''
             else:
-                middle_fileds += f"{' '* (7 - len(str(i+1)))}{i+1}.{' ' * 5}|"
+                middle_fileds += f"{' '* (7 - len(str(i)))}{i}.{' ' * 5}|"
             
             for field in list_of_fields:
+
                 if field == '0':
-                    middle_fileds += '   '
+                    middle_fileds += ' '
                 else:
-                    middle_fileds = middle_fileds + ' ' + field + ' '
-         
-            if i == len(self.game_board_in_class)-2:
+                    middle_fileds += field
+            
+            if i == 0:
+                middle_fileds += ''
+            elif i == 1:
+                middle_fileds += '|'
+            elif i == len(self.game_board_in_class)-3:
                 middle_fileds += '|_\n'
+            elif i == len(self.game_board_in_class)-2:
+                middle_fileds += ''
             elif i == len(self.game_board_in_class)-1:
-                middle_fileds += '| |'
+                middle_fileds += ''
+
             else:
                 middle_fileds += '|\n'
 
         print(middle_fileds, sep='')
-        # self.bo
-
 
         # BOTTOM
-
-        print(f"{' '*14}{overscore*(self.width*3+3)}\n")
+        print(f"{' '*13}{overscore*(self.width+3)}\n")
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ PLAYER_ICON = '@'
 PLAYER_START_X = 3
 PLAYER_START_Y = 3
 
-BOARD_WIDTH = 10
+BOARD_WIDTH = 20
 BOARD_HEIGHT = 10
 
 
@@ -24,60 +24,84 @@ def create_player():
     pass
 
 
+def validate_move_out_of_border(key_press, my_board):
+    
+    if key_press == 'w':
+        if my_board.pos_x == 1 or (my_board.pos_y == 0 and my_board.pos_x == 0):
+            print('You are not allowed to use W key.')
+            return False
+        else:
+            return True
+
+    if key_press == 's':
+        if my_board.pos_x == my_board.height or (my_board.pos_y == my_board.width and my_board.pos_x == my_board.height):
+            print('You are not allowed to use S key.')
+            return False
+        else:
+            return True
+
+    if key_press == 'a':        
+        for item in my_board.game_board_in_class[1:-1]:
+            if my_board.pos_y == 0:
+                print('You are not allowed to use A key.')
+                return False
+        return True
+
+    if key_press == 'd':
+        if my_board.pos_x == 0 and my_board.pos_y == 0:
+            return True
+        if my_board.pos_x == my_board.width-1 and my_board.pos_y == my_board.height:
+            return True
+        for item in my_board.game_board_in_class[1:-1]:
+            if my_board.pos_y == my_board.height:
+                print('You are not allowed to use D key.')
+                return False
+        return True
+
+
 def main():
 
-    # print(f'''
-    #          1. New game
-    #          2. Intro
-    #          3. Hall of flame
-    #          4. About\n''') 
-    # For now just prototype of menu
     my_board = game_board.Board(BOARD_WIDTH, BOARD_HEIGHT, PLAYER_ICON)
-    my_board.create_board()
-    print(my_board.game_board_in_class)
-    x = 0
-    y = 0
-    # player_position
-    # sign_on_map = 'X'
-    # my_board.update_board(x, y)
-    # my_board.print_board()
-
+    x = my_board.pos_x
+    y = my_board.pos_y
     while True:
-        # if my_board.pos_x is None:
-        #     sign_on_map = ''
+
         my_board.print_board()
         my_board.remove_player_track()
-        if util.key_pressed() == 'p':
-            exit(0)
-        if util.key_pressed() == 'w':
-            x -= 1
-            my_board.update_board(x, y)
-            print('UPUPUP')
-            
-        elif util.key_pressed() == 's':
-            x += 1
-            my_board.update_board(x, y)
-            print('DOWN')
-            
-        elif util.key_pressed() == 'a':
-            y -= 1
-            my_board.update_board(x, y)
-            print('LEFT')
-            
-        elif util.key_pressed() == 'd':
-            y += 1
-            my_board.update_board(x, y)
-            print('RIGHT')
+        key_press = util.key_pressed()
+        validation_move = validate_move_out_of_border(key_press, my_board)
 
-        
+        if validation_move is False:
+            my_board.update_board(x, y)
+        else:
+            if key_press == 'p':
+                exit(0)
+            elif key_press == 'w':
+                x -= 1
+                my_board.update_board(x, y)
+                
+            elif key_press == 's':
+                x += 1
+                my_board.update_board(x, y)
+                
+            elif key_press == 'a':
+                y -= 1
+                my_board.update_board(x, y)
+                
+            elif key_press == 'd':
+                if x == 0 and y == 0:
+                    x += 1
+                    my_board.update_board(x, y)
+                elif x == my_board.width-1 and y == my_board.height:
+                    x = len(my_board.game_board_in_class)-1
+                    y = 0
+                    my_board.update_board(x, y)
+                else:
+                    y += 1
+                    my_board.update_board(x, y)
+
+        time.sleep(0.3) 
         util.clear_screen()
-
-
-
-
-
-
-    # print(my_board.game_board_in_class)
 
 
     # util.clear_screen()
@@ -96,3 +120,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
