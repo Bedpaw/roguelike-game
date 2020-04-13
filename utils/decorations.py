2aa3541b3b44e4803454721c34a664cfe77472f7
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-04-13 11:15:40 +0200

    uppercase colors in macro and add docstring to cprint

--- utils/decorations.py
+++ utils/decorations.py
@@ -1,4 +1,13 @@
+from macros.COLORS import *
+
+
 def cprint(string, *args):
+    """
+
+    :param string:[string] string to add styles
+    :param args:[string with ansi]:[tuple strings with ansi] ALWAYS USE MACROS.COLORS WHERE ANSI IS DECODED
+    :return: print(string) and pass
+    """
     reset = '\u001b[0m'
     styles = ''
     for style in args:
@@ -8,3 +17,10 @@ def cprint(string, *args):
         else:
             styles += style
     print(f'{styles}{string}{reset}')
+    pass
+
+# EXAMPLES
+
+# cprint("XD", COLOR.GREEN)
+# cprint("XD", COLOR.WHITE, BG_COLOR.RED, STYLES.BOLD)
+# cprint("XD", ERROR)  # Error is tuple with styles
