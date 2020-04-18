b160b43e41e7bbad7ebe18e74ccdb305311d0cd3
Pati <ppeczynska@gmail.com>
2020-04-18 20:03:11 +0200

    added scheme of inventory

--- db/inventory/inventory.py
+++ db/inventory/inventory.py
@@ -1,1 +1,15 @@
+from utils import decorations
 
+# after player press pause
+# ascii art inventory
+
+
+def print_inventory():
+    pass
+
+
+def add_item():
+    pass
+
+def show_exp_and_lvl():
+    pass
