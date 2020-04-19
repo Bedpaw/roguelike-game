77bd7fc35695b80a8069c913422c46fcbcc945b0
wojciechjsky <jablonski91w@gmail.com>
2020-04-19 20:19:27 +0200

    Preparation

---
+++ classes/Board/test_draw.py
@@ -0,0 +1,19 @@
+
+# symbols_of_map = {
+#     #Grass
+#     '0' : {
+#         'bg_color': '\033[42m',
+#     },
+#     #river
+#     '1' = {
+#         'bg_color': '\033[34m',
+#     },
+#     # bridge
+#     '3' = {
+#         'bg_color':'\033[31m
+#     }
+# }
+
+with open('test_draw.py', 'r') as f:
+    full_map = f.readlines()
+    print(full_map)
