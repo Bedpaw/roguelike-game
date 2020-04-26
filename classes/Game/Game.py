590c5170dc7354e538ec495de621c5ad527d626c
Pawel Bednarczyk <bedpaw97@gmail.com>
2020-04-26 15:11:42 +0200

    Fix bugs with loading game in class Game

--- classes/Game/Game.py
+++ classes/Game/Game.py
@@ -2,6 +2,7 @@ from classes.Board import GameBoard
 from classes.Object.Creature.Hero.Hero import Hero
 from games_config.board_factory import create_new_board
 from utils.data_manager import *
+from time import sleep
 
 
 class Game:
@@ -17,13 +18,11 @@ class Game:
         self.turn_counter = 0
         self.difficulty_level = difficulty_level
         self.endgame = False    # to toogle, when hero/final boss dead
-        self.counter = 1  # test only
         self.board_changed = False
-        self.last_map_pos = []
+        self.true_player_position = []  # for loading data
 
     def next_board(self):
         self.current_board_index += 1
-        self.last_map_pos = [self.hero.position_x, self.hero.position_y]
         self.board_changed = True
         self.hero.position_x = 0
         self.hero.position_y = 0
@@ -31,10 +30,12 @@ class Game:
             self.boards.append(create_new_board(self, self.current_board_index))
 
     def previous_board(self):
-        self.hero.position_x = self.last_map_pos[0]
-        self.hero.position_y = self.last_map_pos[1] - 1  # out of range
-        self.board_changed = True
         self.current_board_index -= 1
+        self.hero.position_x = self.current_board().height
+        self.hero.position_y = self.current_board().width - 1  # out of range
+        self.board_changed = True
+        self.current_board().pos_x = self.hero.position_x
+        self.current_board().pos_y = self.hero.position_y
 
     def current_board(self):
         return self.boards[self.current_board_index]
